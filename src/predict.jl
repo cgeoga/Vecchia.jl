fb55403f64eeab619a492440d2f9b7324064dbc7
Chris Geoga <geoga@wisc.edu>
2026-01-27 15:33:46 -0600

    Yet another point release, this time because the `predict` routines were hitting some crazy slow branch because of the `UpperTriangular` wrapper. Scary.

--- src/predict.jl
+++ src/predict.jl
@@ -1,10 +1,10 @@
 
-struct VecchiaPrediction{M,D,F}
+struct VecchiaPrediction{M,P,F}
   n::Int64
   meanfun::M
   kernel::F
   data::Matrix{Float64} # of size n \times k!
-  joint_pts::Vector{SVector{D,Float64}}
+  joint_pts::Vector{P}
   joint_condix::Vector{Vector{Int64}}
   pred_perm::Vector{Int64}
 end
@@ -12,10 +12,10 @@ end
 default_pred_conditioning(pts) = KNNConditioning(60, Euclidean())
 
 # >= v0.12: updating this internal routine to just have one conditioning argument. 
-function VecchiaPrediction(va::SingletonVecchiaApproximation{M,D,F},
-                           pred_pts::Vector{SVector{D,Float64}};
+function VecchiaPrediction(va::SingletonVecchiaApproximation{M,P,F},
+                           pred_pts::Vector{P};
                            ordering=default_ordering(pred_pts),
-                           conditioning=default_pred_conditioning(pred_pts)) where{M,D,F}
+                           conditioning=default_pred_conditioning(pred_pts)) where{M,P,F}
   isnothing(va.data) && throw(error("For prediction problems, you must provide data."))
   # re-order the points according to the requested ordering.
   (pred_perm, pred_pts_perm, _) = permute_points_and_data(pred_pts, nothing, ordering)
@@ -43,12 +43,12 @@ Keyword arguments are given as:
 - `ordering::PointEnumeration=default_ordering(pred_pts)`: the way you want the prediction points to be ordered. The default in 1D is `Sorted1D()`, and in 2+D is `RandomOrdering()`.
 - `conditioning::ConditioningSetDesign=default_pred_data_conditioning(pred_pts)`: The size of the conditioning sets for the prediction problem. In general, this will be larger than what is used for parameter estimation, and will default to `k=60` nearest neighbors.
 """
-function predict(va::SingletonVecchiaApproximation{M,D,F},
-                 pred_pts::Vector{SVector{D,Float64}}, 
+function predict(va::SingletonVecchiaApproximation{M,P,F},
+                 pred_pts::Vector{P}, 
                  params;
                  conditional_simulate=false,
                  ordering=default_ordering(pred_pts),
-                 conditioning=default_pred_conditioning(pred_pts)) where{M,D,F}
+                 conditioning=default_pred_conditioning(pred_pts)) where{M,P,F}
   vp = VecchiaPrediction(va, pred_pts; ordering, conditioning)
   predict(vp, params)
 end
@@ -58,12 +58,12 @@ function predict(va::ChunkedVecchiaApproximation{M,D,F},
   throw(error("Prediction with `ChunkedVecchiaApproximation`s is not implemented, sorry. It wouldn't be hard, though, so please open an issue if you need that functionality."))
 end
 
-function predict(vp::VecchiaPrediction{M,D,F}, cov_params, mean_params; 
-                 conditional_simulate=false) where{M,D,F}
+function predict(vp::VecchiaPrediction{M,P,F}, cov_params, mean_params; 
+                 conditional_simulate=false) where{M,P,F}
   jva = SingletonVecchiaApproximation(vp.meanfun, vp.kernel, [NaN;;], 
                                       vp.joint_pts, vp.joint_condix, Int64[])
   ixs     = (vp.n+1):length(vp.joint_pts)
-  U       = rchol(jva, cov_params).U
+  U       = rchol(jva, cov_params).U.data
   U_cross = U[(1:vp.n),ixs]
   U_pred  = UpperTriangular(U[ixs, ixs])
   mu      = [jva.meanfun(x, mean_params) for x in jva.pts]
@@ -73,13 +73,12 @@ function predict(vp::VecchiaPrediction{M,D,F}, cov_params, mean_params;
   cmean[invperm(vp.pred_perm)] 
 end
 
-function predict(vp::VecchiaPrediction{M,D,F}, params; 
-                 conditional_simulate=false) where{M,D,F}
+function predict(vp::VecchiaPrediction, params; conditional_simulate=false)
   predict(vp, params, params; conditional_simulate=conditional_simulate)
 end
 
-function predict(vp::VecchiaPrediction{M,D,F}, params::Parameters; 
-                 conditional_simulate=false) where{M,D,F}
+function predict(vp::VecchiaPrediction, params::Parameters; 
+                 conditional_simulate=false)
   predict(vp, params.cov_params, params.mean_params; 
           conditional_simulate=conditional_simulate)
 end
