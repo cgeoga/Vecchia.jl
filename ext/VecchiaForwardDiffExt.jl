87cb7e1e23da03bdeb8c6ede75d81e60d9c29e77
Chris Geoga <geoga@wisc.edu>
2026-03-06 15:47:32 -0600

    Non-allocating term function and buffer struct in expected Fisher matrix. Now just need to write parallel logic.

--- ext/VecchiaForwardDiffExt.jl
+++ ext/VecchiaForwardDiffExt.jl
@@ -65,9 +65,26 @@ module VecchiaForwardDiffExt
     DiffResults.hessian(res)
   end
 
-  # TODO (cg 2026/03/06 15:17): Make non-allocating.
+  struct ExpectedFisherBuf
+    grad::Vector{Float64}
+    fish::Matrix{Float64}
+    primal_cvar_buf::Matrix{Float64}
+    kwts_jac_buf::Matrix{Float64}
+    primal_U_kwts_jac_buf::Matrix{Float64}
+  end
+
+  function ExpectedFisherBuf(cfg, params)
+    grad       = zeros(length(params))
+    fish       = zeros(length(params), length(params))
+    kwts_jac   = zeros(maximum(length, cfg.condix), length(params))
+    U_kwt_jac  = zeros(maximum(length, cfg.condix), length(params))
+    primal_cc  = zeros(maximum(length, cfg.condix), maximum(length, cfg.condix))
+    ExpectedFisherBuf(grad, fish, primal_cc, kwts_jac, U_kwt_jac)
+  end
+
+
   function _efish_term(cfg::Vecchia.SingletonVecchiaApproximation{Vecchia.ZeroMean,P,F}, 
-                       cbuf, grad, fish, params, j::Int) where{P,F}
+                       cbuf, ebuf, params, j::Int) where{P,F}
     # compute the conditional variance and kriging weights for each point.
     cj   = cfg.condix[j]
     ptj  = cfg.pts[j]
@@ -81,20 +98,41 @@ module VecchiaForwardDiffExt
     cvar_parts = ForwardDiff.partials(cvar)
     cvar_value = ForwardDiff.value(cvar)
     for k in 1:length(params)
-      grad[k] += grad_parts[k]
+      ebuf.grad[k] += grad_parts[k]
       for l in 1:length(params)
-        fish[k,l] += (cvar_parts[l]*cvar_parts[k]/abs2(cvar_value))/2
+        ebuf.fish[k,l] += (cvar_parts[l]*cvar_parts[k]/abs2(cvar_value))/2
       end
     end
     # And if the conditioning set isn't empty, we _also_ need to add the
     # quadratic form with Kriging weight jacobian and the _primal_ of the
     # conditional variance.
     if !isempty(cj)
-      cond_var = view(cbuf.buf_cc, 1:length(cj), 1:length(cj))
-      primal_cond_var_U = UpperTriangular(ForwardDiff.value.(cond_var))
-      kwts_jac = [ForwardDiff.partials(kwts[j])[k] for j in 1:length(cj), k in 1:length(params)]
-      tmp      = primal_cond_var_U*kwts_jac
-      fish   .+= (tmp'*tmp)./cvar_value
+      # update the buffer for the Jacobian of the Kriging weights.
+      kwts_jac = view(ebuf.kwts_jac_buf, 1:length(cj), 1:length(params))
+      for j in 1:size(kwts_jac, 1)
+        for k in 1:size(kwts_jac, 2)
+          kwts_jac[j,k] = ForwardDiff.partials(kwts[j])[k]
+        end
+      end
+      # update the buffer for the upper triangle of the _primal_ marginal
+      # covariance of the prediction points.
+      #
+      # BUG: if I use UpperTriangular(primal_cc) to exploit that structure, the
+      # first mul!(...) call below allocates 8 bytes. Not the first time upper
+      # triangular muls/solves have had a trailing allocation issue for me, even
+      # within just this package. LinearAlgebra.jl clearly has a problem
+      # somewhere with triangular matrices.
+      primal_cc = view(ebuf.primal_cvar_buf, 1:length(cj), 1:length(cj))
+      fill!(primal_cc, 0.0)
+      cond_var  = cbuf.buf_cc
+      for k in 1:length(cj)
+        for j in 1:k
+          primal_cc[j,k] = ForwardDiff.value(cond_var[j,k])
+        end
+      end
+      primal_U_kwts = view(ebuf.primal_U_kwts_jac_buf, 1:length(cj), 1:length(params))
+      mul!(primal_U_kwts, primal_cc, kwts_jac)
+      mul!(ebuf.fish, primal_U_kwts', primal_U_kwts, inv(cvar_value), 1.0)
     end
     nothing
   end
@@ -103,13 +141,12 @@ module VecchiaForwardDiffExt
   function _efish(cfg::Vecchia.SingletonVecchiaApproximation{Vecchia.ZeroMean,P,F}, 
                   params) where{P,F}
     size(cfg.data, 2) == 1 || throw(error("Only for m=1 right now, sorry."))
-    fish  = zeros(length(params), length(params))
-    grad  = zeros(length(params))
-    cbuf  = Vecchia.cnllbuf(cfg, params)
+    cbuf = Vecchia.cnllbuf(cfg, params)
+    ebuf = ExpectedFisherBuf(cfg, params)
     for j in 1:length(cfg.condix) 
-      _efish_term(cfg, cbuf, grad, fish, params, j)
+      _efish_term(cfg, cbuf, ebuf, params, j)
     end
-    (grad, fish) 
+    (ebuf.grad, ebuf.fish) 
   end
 
   function Vecchia.efish(cfg::R, params::Vector{Float64}) where{R}
